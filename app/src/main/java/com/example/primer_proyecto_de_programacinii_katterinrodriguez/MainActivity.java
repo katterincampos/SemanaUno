609aabb4f182be0bbf1f9fb29059c0b93a92da86
katterincampos <88002663+katterincampos@users.noreply.github.com>
2022-02-01 15:39:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/primer_proyecto_de_programacinii_katterinrodriguez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.primer_proyecto_de_programacinii_katterinrodriguez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
